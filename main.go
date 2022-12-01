5dfda8c1ee7528258420bae625047756dfff6a59
Grey <grey0520@foxmail.com>
2022-12-01 19:57:19 +0800

    添加自定义的Logger

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
     "fmt"
 
     "github.com/Grey0520/isnip_api/settings"
+    "github.com/Grey0520/isnip_api/logger"
 )
 func main() {
     // 从`conf/conf.yaml`加载配置信息
     if err := settings.Init(); err != nil {
         fmt.Printf("load config failed, err:%v\n", err)
+        return
+    }
+    // 自定义logger
+    if err := logger.Init(settings.Conf.LogConfig, settings.Conf.Mode); err != nil {
+        fmt.Printf("init logger failed, err:%v\n", err)
         return
     }
 }
