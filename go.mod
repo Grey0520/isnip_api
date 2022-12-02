731ce9e3d376be6f6943ee7208275a7ab7471345
Grey <grey0520@foxmail.com>
2022-12-02 20:46:32 +0800

    添加mysql的连接

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.18
 require (
 	github.com/fsnotify/fsnotify v1.6.0
 	github.com/gin-gonic/gin v1.8.1
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.5
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/spf13/viper v1.14.0
 	go.uber.org/zap v1.21.0
--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/Grey0520/isnip_api/settings"
-    "github.com/Grey0520/isnip_api/logger"
+	"github.com/Grey0520/isnip_api/logger"
+	"github.com/Grey0520/isnip_api/settings"
+    "github.com/Grey0520/isnip_api/dao/mysql"
 )
+
 func main() {
-    // 从`conf/conf.yaml`加载配置信息
-    if err := settings.Init(); err != nil {
-        fmt.Printf("load config failed, err:%v\n", err)
-        return
-    }
-    // 自定义logger
-    if err := logger.Init(settings.Conf.LogConfig, settings.Conf.Mode); err != nil {
-        fmt.Printf("init logger failed, err:%v\n", err)
-        return
-    }
+	// 从`conf/conf.yaml`加载配置信息
+	if err := settings.Init(); err != nil {
+		fmt.Printf("load config failed, err:%v\n", err)
+		return
+	}
+	// 自定义logger
+	if err := logger.Init(settings.Conf.LogConfig, settings.Conf.Mode); err != nil {
+		fmt.Printf("init logger failed, err:%v\n", err)
+		return
+	}
+	// mysql的连接
+	if err := mysql.Init(settings.Conf.MySQLConfig); err != nil {
+		fmt.Printf("init mysql failed, err:%v\n", err)
+		return
+	}
+	defer mysql.Close() // 程序退出关闭数据库连接
 }
